dae0fa2298b79fa139b7e10fa414deed94d7e8dc
Ruben Flores <rflores@MASTEREDI.COM.MX>
2023-11-08 16:54:21 -0600

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/components/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
